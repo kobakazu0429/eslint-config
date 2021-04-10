9a64cb81f6df82ee3869b27a7e242c473a4ae647
kobakakzu0429 <kobakazu10@gmail.com>
2021-04-10 09:35:41 +0900

    change rule to error off that always uses

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -18,6 +18,9 @@ module.exports = {
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/member-ordering": [
       "error",
