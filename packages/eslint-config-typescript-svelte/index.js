889100dbfae7fd6d5d1b73dfac30ef70a74c2ac7
kobakazu0429 <kobakazu10@gmail.com>
2023-11-22 12:58:01 +0900

    fix

--- packages/eslint-config-typescript-svelte/index.js
+++ packages/eslint-config-typescript-svelte/index.js
@@ -2,17 +2,16 @@ const typescriptOptions = require("@kobakazu0429/eslint-config-typescript");
 
 module.exports = {
   ...typescriptOptions,
-  plugins: [...typescriptOptions.plugins, "prettier-plugin-svelte"],
   extends: [
     ...typescriptOptions.extends,
     "plugin:svelte/recommended",
-    "plugin:svelte/prettier",
+    "plugin:svelte/prettier"
   ],
   parserOptions: {
     ...typescriptOptions.parserOptions,
     tsconfigRootDir: __dirname,
     project: ["./tsconfig.json"],
-    extraFileExtensions: [".svelte"], // This is a required setting in `@typescript-eslint/parser` v4.24.0.
+    extraFileExtensions: [".svelte"] // This is a required setting in `@typescript-eslint/parser` v4.24.0.
   },
   overrides: [
     {
@@ -20,16 +19,16 @@ module.exports = {
       parser: "svelte-eslint-parser",
       // Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
       parserOptions: {
-        parser: "@typescript-eslint/parser",
-      },
-    },
+        parser: "@typescript-eslint/parser"
+      }
+    }
   ],
   rules: {
     ...typescriptOptions.rules,
     // for svelte
     "@typescript-eslint/consistent-type-imports": [
       "error",
-      { prefer: "type-imports" },
-    ],
-  },
+      { prefer: "type-imports" }
+    ]
+  }
 };
